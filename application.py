5f5b82aa0be33188cef4181fbad71e7de2a98283
rajak2025 <41680911+rajak2025@users.noreply.github.com>
2020-10-10 11:23:46 +0530

    Created template directory

--- application.py
+++ application.py
@@ -3,5 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def indexmain():
     return render_template("index.html")
+
+
+@app.route("/<string:name>")
+def index(name):
+    return render_template('index.html', name = name.capitalize())
